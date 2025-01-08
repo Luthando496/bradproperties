0cca975c12c16f9c9207fb4e5f804034e8f1aeb2
luthando496 <luthandodidiza197@gmail.com>
2025-01-08 11:37:25 -0800

    file routing basics

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 const HomePage = () => {
   return (
-    <section className='h-screen bg-amber-500'>
-      HomePage
+    <section className='h-screen bg-amber-500 flex w-full gap-36'>
+      <Link href={'/properties'}>Properties</Link>
+      <h1> HomePage </h1> 
     </section>
   )
 }
